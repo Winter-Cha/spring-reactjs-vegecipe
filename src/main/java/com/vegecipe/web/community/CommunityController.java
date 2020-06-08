59e96797298edc62764a0e35ed07da03c59a88ab
Winter-mac-window <diskdooencl@gmail.com>
2020-06-08 21:57:09 +0900

    change controller

--- src/main/java/com/vegecipe/web/community/CommunityController.java
+++ src/main/java/com/vegecipe/web/community/CommunityController.java
@@ -4,6 +4,7 @@ import com.vegecipe.service.community.PostsService;
 import com.vegecipe.dto.community.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,11 +16,10 @@ public class CommunityController {
     private final PostsService postsService;
 
     @GetMapping("/community")
-    public ModelAndView community() {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("/pages/community/posts_list");
-        modelAndView.addObject("posts", postsService.findAllDesc());
-        return modelAndView;
+    public String community(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
+        //return "/pages/community/posts_list";
+        return "/pre_page";
     }
 
     @GetMapping("/posts/write")
@@ -28,12 +28,10 @@ public class CommunityController {
     }
 
     @GetMapping("/posts/update/{id}")
-    public ModelAndView postUpdate(@PathVariable Long id) {
+    public String postUpdate(@PathVariable Long id, Model model) {
         PostsResponseDto dto = postsService.findById(id);
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("/pages/community/posts_update");
-        modelAndView.addObject("post", dto);
-        return modelAndView;
+        model.addAttribute("post", dto);
+        return "/pages/community/posts_update";
     }
 
 }
