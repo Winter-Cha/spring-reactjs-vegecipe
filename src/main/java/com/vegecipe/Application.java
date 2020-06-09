f2cc28aa7304dbe408e834ae100f10985d706ed4
Winter-Cha <diskdooencl@gmail.com>
2020-06-09 11:32:48 +0900

    Update Application.java

--- src/main/java/com/vegecipe/Application.java
+++ src/main/java/com/vegecipe/Application.java
@@ -2,11 +2,18 @@ package com.vegecipe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
+    
+    @Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
+	}
+    
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
