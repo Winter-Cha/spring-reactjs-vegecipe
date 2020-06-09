13de3e27c99b654011972d10ff98511ac4bb949f
Winter-mac-window <diskdooencl@gmail.com>
2020-06-10 01:02:47 +0900

    test 4

--- src/main/java/com/vegecipe/web/IndexController.java
+++ src/main/java/com/vegecipe/web/IndexController.java
@@ -38,45 +38,6 @@ public class IndexController {
         return "pages/login";
     }
 
-    private final BooksService booksService;
 
-    @GetMapping("/book")
-    //public String book(Model model) {
-    public String book() {
-        //model.addAttribute("books", booksService.findAllDesc());
-        return "pages/book/books_list";
-    }
-
-    @GetMapping("/book/write")
-    public String bookWrite() {
-        return "pages/book/books_write";
-    }
-
-    @GetMapping("/book/update/{id}")
-    public String bookUpdate(@PathVariable Long id, Model model) {
-        BooksResponseDto dto = booksService.findById(id);
-        model.addAttribute("book", dto);
-        return "pages/book/books_update";
-    }
-
-    @GetMapping("/book/view/{id}")
-    public String bookView(@PathVariable Long id, @LoginUser SessionUser user, Model model ) {
-        BooksResponseDto dto = booksService.findById(id);
-        booksService.updateViewCnt(id);
-        BooksResponseDto pre = booksService.findByIdPreBook(id);
-        BooksResponseDto post = booksService.findByIdPostBook(id);
-
-        model.addAttribute("book", dto);
-
-        // author check
-        String authorEmail = dto.getAuthorEmail();
-        if(user != null && user.getEmail().equals(authorEmail))
-            model.addAttribute("isAuthor", true);
-
-        model.addAttribute("pre_book", pre);
-        model.addAttribute("post_book", post);
-
-        return "pages/book/books_view";
-    }
 
 }
