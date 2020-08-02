408de665082d1d5664f30f3d24ed6feb1c71d391
Winter-mac-window <diskdooencl@gmail.com>
2020-08-02 15:55:26 +0900

    다음글 쿼리 변경

--- src/main/java/com/vegecipe/domain/book/BooksRepository.java
+++ src/main/java/com/vegecipe/domain/book/BooksRepository.java
@@ -10,10 +10,10 @@ public interface BooksRepository extends JpaRepository<Books,Long> {
     @Query("SELECT b FROM Books b ORDER BY b.id DESC")
     List<Books> findAllDesc();
 
-    @Query(value = "SELECT * FROM books b WHERE b.id > ?1 ORDER BY b.id DESC LIMIT 1", nativeQuery = true )
+    @Query(value = "SELECT * FROM books b WHERE b.id > ?1 ORDER BY b.id LIMIT 1", nativeQuery = true )
     Books findByIdPreBook(Long id);
 
-    @Query(value = "SELECT * FROM books b WHERE b.id < ?1 ORDER BY b.id LIMIT 1", nativeQuery = true )
+    @Query(value = "SELECT * FROM books b WHERE b.id < ?1 ORDER BY b.id DESC LIMIT 1", nativeQuery = true )
     Books findByIdPostBook(Long id);
 
     @Query(value = "UPDATE Books b SET b.VIEW_CNT = (SELECT s.VIEW_CNT FROM Books s WHERE s.id = ?1) +1 WHERE b.id = ?1", nativeQuery = true )
